86a280c0eaf2084cf093d0c3a469086a992818fa
Ben Jones <benpaddlejones@outlook.com>
2025-10-22 23:33:42 +0000

    fix typo

--- main.py
+++ main.py
@@ -5,20 +5,23 @@ import database_manager as dbHandler
 
 app = Flask(__name__)
 
-@app.route('/index.html', methods=['GET'])
-@app.route('/', methods=['POST', 'GET'])
+
+@app.route("/index.html", methods=["GET"])
+@app.route("/", methods=["POST", "GET"])
 def index():
-     return render_template('/index.html', content=dbHandler.listExtension())
+    return render_template("/index.html", content=dbHandler.listExtension())
 
-@app.route('/add.html', methods=['POST', 'GET'])
+
+@app.route("/add.html", methods=["POST", "GET"])
 def add():
-	if request.method=='POST':
-		email = request.form['email']
-		name = request.form['name']
-		dbHandler.insertContact(email,name)
-		return render_template('/add.html', message="Thank you for signing up")
-	else:
-		return render_template('/add.html')
+    if request.method == "POST":
+        email = request.form["email"]
+        name = request.form["name"]
+        dbHandler.insertContact(email, name)
+        return render_template("/add.html", is_done=True)
+    else:
+        return render_template("/add.html")
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
